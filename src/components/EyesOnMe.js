ac4eb403c29e5cbd091f592d05984f57d9c3fc2f
Louis Y <louisy603@hotmail.com>
2022-04-27 22:45:49 -0400

    commit tree 1929ef882ea088f09b4a3a3dd1d4df0524fe653a

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function clicker (e) {
+    console.log('Good!')
+}
+
+function blur(e) {
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe() {
+  return (
+    <button onFocus={clicker} onBlur={blur}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function passwordChange() {
+    console.log("Entering password...")
+}
+
+function Keypad() {
+  return (
+    <form>
+        <label>Password</label>
+        <input 
+            type="password"
+            onChange={passwordChange}
+        ></input>
+    </form>
+  )
+}
+
+export default Keypad
